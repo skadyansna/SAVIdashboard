2003923793a1baaa027d043d946848f4e7765119
harshuvic <harshdawar10@gmail.com>
2015-09-29 00:15:37 -0700

    D3 Data binding changed

--- view/example/data.js
+++ view/example/data.js
@@ -4,36 +4,33 @@ var shouter = new ko.subscribable();
 
 
 var d3Model =  function() {
-    self=this
+    var self = this;
     self.cpu = ko.observable("NA");
     self.cpuUtilData = ko.observable("NA");
     // Main Model
-    //function updateServerdata(serverSpecificData){
-    //    self.cpu = serverSpecificData.cpuData
-    //    self.cpu_util = serverSpecificData.cpu_Util;
-    //    self.diskephemeralsize = serverSpecificData.diskephemeralSize;
-    //    self.diskreadbyte = serverSpecificData.diskreadBytes;
-    //    self.diskrootsize = serverSpecificData.diskrootSize;
-    //    self.diskwritebytes = serverSpecificData.diskwriteBytes;
-    //    self.diskwriterequests = serverSpecificData.diskwriteRequests;
-    //    self.instance = serverSpecificData.instance;
-    //    self.memory = serverSpecificData.Memory;
-    //    self.vcpus = serverSpecificData.vcpus;
-    //    self.networkincomingbytes = serverSpecificData.networkincomingBytes;
-    //    self.networkoutgoingbytes = serverSpecificData.networkoutgoingBytes;
-    //}
+    function updateServerdata(serverSpecificData){
+        self.cpu(serverSpecificData.cpuData);
+        self.cpuUtilData(serverSpecificData.cpu_Util);
+        self.diskephemeralsize = serverSpecificData.diskephemeralSize;
+        self.diskreadbyte = serverSpecificData.diskreadBytes;
+        self.diskrootsize = serverSpecificData.diskrootSize;
+        self.diskwritebytes = serverSpecificData.diskwriteBytes;
+        self.diskwriterequests = serverSpecificData.diskwriteRequests;
+        self.instance = serverSpecificData.instance;
+        self.memory = serverSpecificData.Memory;
+        self.vcpus = serverSpecificData.vcpus;
+        self.networkincomingbytes = serverSpecificData.networkincomingBytes;
+        self.networkoutgoingbytes = serverSpecificData.networkoutgoingBytes;
+    }
     shouter.subscribe(function(serverName) {
-
-        //API call to get fresh server data....
         $.ajax({
             type: 'GET',
             data: serverName,
             contentType: "application/json",
-            //contentType: "application/x-www-form-urlencoded",
             dataType:'json',
             url:"http://localhost:3000/resourceNameInverse",
             success: function(data) {
-                //updateServerdata(data);
+                updateServerdata(data);
                 console.log(data);
             },
             error: function(error) {
@@ -41,9 +38,7 @@ var d3Model =  function() {
             }
         });
         console.log('D3 Model Refreshed');
-        self.cpu(Math.random() * 100);
-        self.cpuUtilData(Math.random() * 100);
-    }, self, "messageToPublish");
+    }, this, "messageToPublish");
 };
 var serverDataModel = function () {
     this.message = ko.observable("This represents the server data.....");
@@ -63,15 +58,6 @@ var resourceListModel = function(resourceList) {
         console.log('newValue');
     });
 };
-
-/*var testModel = function(resourceList) {
-    testOptions = ko.observableArray();
-    var resourceListArray = Object.keys(resourceList).map(function (key) {return resourceList[key]});
-    for(var resourceIndex in resourceListArray) {
-        testOptions.push(resourceListArray[resourceIndex]);
-    }
-    testData = ko.observable();
-};*/
 
 var getResourceIDData = function (resourceName) {
     console.log('Selected Value ' + resourceName);
